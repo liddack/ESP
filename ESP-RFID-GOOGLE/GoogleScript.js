ea72ba8f1c6d10574e83c1790bae88d68249298b
Lucas Miranda <liddack@outlook.com>
2021-03-17 17:54:14 -0300

    Scripts traduzidos para PT-BR

--- ESP-RFID-GOOGLE/GoogleScript.js
+++ ESP-RFID-GOOGLE/GoogleScript.js
@@ -1,108 +1,105 @@
+var timeZone = "BRT";
+  var dateTimeFormat = "dd/MM/yyyy HH:mm:ss";
+  var logSpreadSheetId = "";
 
-var timeZone="CST";
-var dateTimeFormat="dd/MM/yyyy HH:mm:ss";
-var logSpreadSheetId="";
-
-function sendEmail(message, id) {
-    var subject = 'Something wrong with ' + id;
+  function sendEmail(message, id) {
+    var subject = 'Algo errado com ' + id;
     MailApp.sendEmail(emailAddress, subject, message);
+  }
 
-}
-
-function doGet(e) {
-   var access="-1";
-      var text='go home';
-      var name='Who are you?';
-  var json;
-  var error="idk";
+  function doGet(e) {
+    var access = "-1";
+    var text = 'boa tentativa';
+    var name = 'Quem é você?';
+    var json;
+    var error = "sei lá";
     Logger.log(JSON.stringify(e)); // view parameters
     var result = 'Ok'; // assume success
     if (e.parameter == 'undefined') {
-        result = 'No Parameters';
+      result = 'Sem parâmetros';
     } else {
 
-        var uid = '';
-        var onlyPing=false;
-        var id = 'headquarter';
-        var error = '';
-        for (var param in e.parameter) {
+      var uid = '';
+      var onlyPing = false;
+      var id = 'Sede';
+      var error = '';
+      for (var param in e.parameter) {
 
-            var value = stripQuotes(e.parameter[param]);
+        var value = stripQuotes(e.parameter[param]);
 
-            switch (param) {
-                case 'uid':
-                    uid = value;
-                    break;
-                case 'id':
-                    id = value;
-                    break;
-       
-                default:
-                    result = "unsupported parameter";
-            }
+        switch (param) {
+          case 'uid':
+            uid = value;
+            break;
+          case 'id':
+            id = value;
+            break;
+
+          default:
+            result = "parâmetro incompatível";
         }
-      
-     
-      var sheet=SpreadsheetApp.getActive().getActiveSheet();
-     
-      var data = sheet.getDataRange().getValues();
-    if (data.length == 0)
-        return;
-    for (var i = 0; i < data.length; i++) {
+      }
 
-        if (data[i][0] ==uid)
-        {
-          name=data[i][1];
-          access=data[i][2];
-          text=data[i][3];
+
+      var sheet = SpreadsheetApp.getActive().getActiveSheet();
+
+      var data = sheet.getDataRange().getValues();
+      if (data.length == 0)
+        return;
+      for (var i = 0; i < data.length; i++) {
+
+        if (data[i][0] == uid) {
+          name = data[i][1];
+          access = data[i][2];
+          text = data[i][3];
           break;
         }
-       
+
+      }
+
+
+      addLog(uid, id, name, access);
+
+
     }
-      
-      
-      addLog(uid,id,name,access);
-       
-    
-       }
-//     json = {
-//    'access':access, 
-//    'name': name,
-//    'text':text,     
-//    'error':error}
-     
-     result=(access+":"+name+":"+text);
-  return ContentService.createTextOutput(result);
-//  return ContentService.createTextOutput(JSON.stringify(json) ).setMimeType(ContentService.MimeType.JSON); 
-}
+    //     json = {
+    //    'access':access, 
+    //    'name': name,
+    //    'text':text,     
+    //    'error':error}
+
+    result = (access + ":" + name + ":" + text);
+    return ContentService.createTextOutput(result);
+    //  return ContentService.createTextOutput(JSON.stringify(json) ).setMimeType(ContentService.MimeType.JSON); 
+  }
 
 
-function addLog(uid,entrance, name,result) {
+  function addLog(uid, entrance, name, result) {
 
-    
-    var spr=SpreadsheetApp.openById(logSpreadSheetId);
+
+    var spr = SpreadsheetApp.openById(logSpreadSheetId);
     var sheet = spr.getSheets()[0];
     var data = sheet.getDataRange().getValues();
-   
+
     var pos = sheet.getLastRow() + 1;
 
     var rowData = [];
-  rowData[0] = Utilities.formatDate(new Date(), timeZone, dateTimeFormat);
-  rowData[4]=entrance;
-  rowData[1] = uid;
-  rowData[2] = name;
-  rowData[3] = result;
+    rowData[0] = Utilities.formatDate(new Date(), timeZone, dateTimeFormat);
+    rowData[4] = entrance;
+    rowData[1] = uid;
+    rowData[2] = name;
+    rowData[3] = result;
     var newRange = sheet.getRange(pos, 1, 1, rowData.length);
     newRange.setValues([rowData]);
 
 
-}
+  }
 
 
 
-/**
- * Remove leading and trailing single or double quotes
- */
-function stripQuotes(value) {
+  /**
+   * Remove leading and trailing single or double quotes
+   */
+  function stripQuotes(value) {
     return value.replace(/^["']|['"]$/g, "");
-}
+  }
--- ESP-RFID-GOOGLE/RFIDGOOGLE.ino
+++ ESP-RFID-GOOGLE/RFIDGOOGLE.ino
@@ -7,22 +7,22 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 #define SS_PIN D4
-#define RST_PIN D0    // Configurable, see typical pin layout above
+#define RST_PIN D0    // Configurável, veja a pinagem padrão acima
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Criando instância do MFRC522
 #define BUZZ_PIN D8
 #define GATE_PIN D3
 const char* host = "script.google.com";
 const int httpsPort = 443;
-const char* fingerprint  = "46 B2 C3 44 9C 59 09 8B 01 B6 F8 BD 4C FB 00 74 91 2F EF F6"; // for https
+const char* fingerprint  = "46 B2 C3 44 9C 59 09 8B 01 B6 F8 BD 4C FB 00 74 91 2F EF F6"; // para https
 
 
-//***********Things to change*******************
+//*********** Linhas para alterar *******************
 const char* ssid = "";
 const char* password = "";
 String GOOGLE_SCRIPT_ID = ""; // Replace by your GAS service id
-const String unitName = "headquarter"; // any name without spaces and special characters
-//***********Things to change*******************
+const String unitName = "sede"; // qualquer nome sem espaços nem caracteres especiais
+//*********** Fim das linhas para alterar *******************
 uint64_t openGateMillis = 0;
 WiFiClientSecure client;
 
@@ -79,30 +79,30 @@ void setup() {
 
   
 
-  lcd.begin(); // Init with pin default ESP8266 or ARDUINO
-// lcd.begin(0, 2); //ESP8266-01 I2C with pin 0-SDA 2-SCL
-// Turn on the blacklight and print a message.
+  lcd.begin(); // Inicializa com o pino padrão do ESP8266 ou ARDUINO
+// lcd.begin(0, 2); //ESP8266-01 I2C com o pino 0-SDA 2-SCL
+// Aciona a luz de fundo e exibe uma mensagem.
   lcd.backlight();
-  LcdClearAndPrint("Loading");
+  LcdClearAndPrint("Carregando");
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  Serial.println("Started");
-  Serial.print("Connecting");
+  Serial.println("Iniciado");
+  Serial.print("Conectando");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  // Initialize serial communications with the PC
-  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-  SPI.begin();      // Init SPI bus
-  mfrc522.PCD_Init();   // Init MFRC522
-  delay(4);       // Optional delay. Some board do need more time after init to be ready, see Readme
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
-  LcdClearAndPrint("Ready");
+  // Inicializa a comunicação serial com o PC
+  while (!Serial);    // Não faz nada se nenhuma porta serial estiver aberta (adicionado para Arduinos baseados no ATMEGA32U4)
+  SPI.begin();      // Inicializa o barramento SPI
+  mfrc522.PCD_Init();   // Inicializa o MFRC522
+  delay(4);       // Atraso opcional. Algumas placas precisam de mais tempo depois de inicializar para ficarem prontas.
+  mfrc522.PCD_DumpVersionToSerial();  // Mostra os detalhes do leitor de cartão MFRC522
+  Serial.println(F("Escaneie o PICC para ver UID, SAK, tipo e blocos de dados..."));
+  LcdClearAndPrint("Pronto");
 }
 byte readCard[4];
 
@@ -120,20 +120,20 @@ void HandleDataFromGoogle(String data)
   lcd.print(text);
   if (access=="-1")
   {
-    lcd.print(" " + String("denied"));
+    lcd.print(" " + String("negado"));
     Siren(); 
-    LcdClearAndPrint("Ready");
+    LcdClearAndPrint("Pronto");
   }
   else if(access=="any")
   {
     
-    lcd.print(" " + String("go in"));
+    lcd.print(" " + String("Pode entrar"));
     OpenGate();
   }
-  else if (access=="fridge")
+  else if (access=="geladeira")
   {
     
-    lcd.print(" " + String("take it"));
+    lcd.print(" " + String("Pode pegar"));
     OpenGate();
   }
 
@@ -153,7 +153,7 @@ void CloseGate()
   openGateMillis = 0;
   digitalWrite(GATE_PIN, LOW);
   Beep2();
-  LcdClearAndPrint("Ready");
+  LcdClearAndPrint("Pronto");
 }
 
 void loop() {
@@ -167,12 +167,12 @@ void loop() {
     return;
   }
 
-  // Select one of the cards
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  // Seleciona um dos cartões
+  // Reseta o loop se nenhum novo cartão estiver presente no leitor. Isto evita que o processo inteiro rode novamente quando ocioso.
   if (!mfrc522.PICC_ReadCardSerial()) {
     return;
   }
-  Serial.println(F("Scanned PICC's UID:"));
+  Serial.println(F("UID escaneado do PICC:"));
   String uid = "";
   for (uint8_t i = 0; i < 4; i++) {  //
     readCard[i] = mfrc522.uid.uidByte[i];
@@ -182,7 +182,7 @@ void loop() {
   Serial.println("");
 
   Beep();
-  LcdClearAndPrint("Please wait...");
+  LcdClearAndPrint("Aguarde...");
   String data = sendData("id=" + unitName + "&uid=" + uid,NULL);
   HandleDataFromGoogle(data);
 
@@ -192,7 +192,7 @@ void loop() {
 
 
 String sendData(String params, char* domain) {
-  //google scripts requires two get requests 
+  // O google scripts precisa de duas solicitações GET 
   bool needRedir = false;
   if (domain == NULL)
   {
@@ -204,24 +204,24 @@ String sendData(String params, char* domain) {
   Serial.println(*domain);
   String result = "";
   client.setInsecure();
-  Serial.print("connecting to ");
+  Serial.print("Conectando-se a");
   Serial.println(host);
   if (!client.connect(host, httpsPort)) {
-    Serial.println("connection failed");
+    Serial.println("Falha na conexão");
     return "";
   }
 
   if (client.verify(fingerprint, domain)) {
   }
 
-  Serial.print("requesting URL: ");
+  Serial.print("Solicitando URL: ");
   Serial.println(params);
 
   client.print(String("GET ") + params + " HTTP/1.1\r\n" +
     "Host: " + domain + "\r\n" +
     "Connection: close\r\n\r\n");
 
-  Serial.println("request sent");
+  Serial.println("solicitação enviada");
   while (client.connected()) {
 
     String line = client.readStringUntil('\n');
@@ -241,7 +241,7 @@ String sendData(String params, char* domain) {
     }
 
     if (line == "\r") {
-      Serial.println("headers received");
+      Serial.println("cabeçalhos recebidos");
       break;
     }
   }
@@ -259,4 +259,3 @@ String sendData(String params, char* domain) {
   
 
 }
-
